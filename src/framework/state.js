b5cebd51570ce96203b82fb78fef9335e2b64294
hghomari <hamzaghomari16@gmail.com>
2026-08-29 19:47:02 +0100

    understand how state management work

--- src/framework/state.js
+++ src/framework/state.js
@@ -1,7 +1,20 @@
 
+export let store = {}
+
+export function getValue(name) {
+    Object.entries(store).forEach((funcStore, valueStore) => {
+        if (funcStore === name) {
+            return valueStore
+        }
+    });
+    return null
+}
+
+export function setValue(value, name) {
+        store[name]=value
+}
 
 
-export function createStore(initialState){
 
 
 
@@ -9,6 +22,27 @@ export function createStore(initialState){
 
 
 
+
+
+
+
+
+
+export function createStore(initialState) {
+
+    const getState = () => {
+        return initialState
+    }
+    const changeSate = () => {
+
+
+
+    }
+
+
+    return {
+        getState
+    }
 
 }
 
--- src/todo/app.js
+++ src/todo/app.js
@@ -1,4 +1,14 @@
 
+import { getValue,setValue,store } from "../framework/state.js";
 
+const btn= document.getElementById("btn")
+const count = document.getElementById("text")
+setValue(0,count);
+console.log("store",store[count]);
 
-
+btn.addEventListener("click",()=>{
+    
+    setValue(getValue(count)+1,count)
+    count.textContent=store[count];
+    console.log(store[count]);
+})
