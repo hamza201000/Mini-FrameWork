03d88cb07a6833e0ce50ec563bbdc39442ab9dbc
iallaoui <imranallaoui0@gmail.com>
2026-09-10 14:38:26 +0100

    router simplification

--- src/framework/router.js
+++ src/framework/router.js
@@ -1,54 +1,30 @@
-// single page application (spa)
 class AppRouter {
     constructor(options = {}) {
         this.routes = options.routes || {};
-        this.defaultRoute = options.defaultRoute || '#/';
         this.currentPath = null;
-        this.subscribers = [];
-        this._onHashChange = this.handleRouteChange.bind(this);
-        this.start();
-    }
 
-    start() {
-        window.addEventListener('hashchange', this._onHashChange);
-        if (!window.location.hash) {
-            window.location.hash = this.defaultRoute;
-        } else {
+        window.addEventListener('hashchange', () => {
             this.handleRouteChange();
-        }
+        });
+
+        this.handleRouteChange();
     }
 
     handleRouteChange() {
-        const path = window.location.hash || this.defaultRoute;
+        const path = window.location.hash || '#/';
         this.currentPath = path;
+
         const action = this.routes[path];
 
-        if (action && typeof action === 'function') {
+        if (typeof action === 'function') {
             action();
-            this.notify(action);
         } else {
-            console.warn(`[Router] No route found for: ${path}`);
+            console.warn(`No route found for: ${path}`);
         }
     }
 
     navigate(path) {
         window.location.hash = path;
-    }
-
-    subscribe(callback) {
-        this.subscribers.push(callback);
-        this.handleRouteChange(); 
-        return () => {
-            this.subscribers = this.subscribers.filter(s => s !== callback);
-        };
-    }
-
-    notify(component) {
-        this.subscribers.forEach(cb => cb(component));
-    }
-
-    destroy() {
-        window.removeEventListener('hashchange', this._onHashChange);
     }
 }
 
